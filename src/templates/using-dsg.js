456471f916fe0cc89f234dec0d262f2074697de0
Francis Labi <francislabu@Franciss-MacBook-Pro.local>
2022-11-07 12:25:37 +0100

    -a

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -10,6 +10,7 @@ const UsingDSG = () => (
       Hello from a <b>DSG Page</b>
     </h1>
     <p>This page is not created until requested by a user.</p>
+    <gatsbyImage src="../images/example.png" alt="A dinosaur" width="200"/>
     <p>
       To learn more, head over to our{" "}
       <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
