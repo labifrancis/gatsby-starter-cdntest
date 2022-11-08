c9ef11b448e834f713c0f36e630c121646870c79
Labi Francis <moodyblerd@gmail.com>
2022-11-08 15:37:12 +0100

    Updated image

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -10,8 +10,9 @@ const UsingDSG = () => (
     <h1>
       Hello from a <b>DSG Page</b>
     </h1>
-    <p>This page is not created until requested by a user.</p>
-    <gatsbyImage src="../images/example.png" alt="A dinosaur" width="200"/>
+    <p>This page is not created until requested by a user.
+  <gatsbyImage src="../images/example.png" alt="A dinosaur" width="200"/></p>
+    
     <p>
       To learn more, head over to our{" "}
       <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
